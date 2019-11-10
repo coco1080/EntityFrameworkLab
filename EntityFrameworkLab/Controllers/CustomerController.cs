84b3f4b66e0bae386c701d6582c5818a23d5381a
coco1080 <57575842+coco1080@users.noreply.github.com>
2019-11-10 15:40:16 -0600

    Entuty framework logic (#1)

---
+++ EntityFrameworkLab/Controllers/CustomerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFrameworkLab.Models;
+using System.Data.Entity;
+
+namespace EntityFrameworkLab.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using(DbModels1 dbModel = new DbModels1())
+            {
+                return View(dbModel.Customer.ToList());
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using(DbModels1 dbModel = new DbModels1())
+            {
+                return View(dbModel.Customer.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DbModels1 dbModel = new DbModels1())
+                {
+                    dbModel.Customer.Add(customer);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using(DbModels1 dbModel = new DbModels1())
+                {
+                    dbModel.Entry(customer).State = System.Data.EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DbModels1 dbModel = new DbModels1())
+                {
+                    Customer customer = dbModel.Customer.Where(x => x.Id == id).FirstOrDefault();
+                    dbModel.Customer.Remove(customer);
+                    dbModel.SaveChanges();
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
